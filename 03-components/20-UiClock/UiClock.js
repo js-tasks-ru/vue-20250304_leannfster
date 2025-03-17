be161e08894aa5cbfaf6f56e19f280fa1f0f96be
Daria Myshlennikova <leannfster@gmail.com>
2025-03-17 21:54:39 +0100

    Решает задачу UiClock

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,30 @@
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeMount, onUnmounted, computed, ref } from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() { 
+    let currrentTime = ref();
+    let intervalID = null;
+    const mediumTime = new Intl.DateTimeFormat(navigator.language, {
+      timeStyle: 'medium'
+    });
 
-  template: `<div class="clock">10:12:02</div>`,
+    const getCurrentTime = () => {
+      currrentTime.value = mediumTime.format(new Date())
+    }
+    onBeforeMount(() => {
+      getCurrentTime()
+      intervalID = setInterval(getCurrentTime, 1000)
+    })
+    onUnmounted(() => {
+      clearInterval(intervalID)
+    })
+
+    return {
+      currrentTime
+    }
+  },
+
+  template: `<div class="clock">{{ currrentTime }}</div>`,
 })
