68417989aaa8933eccbd6d715f522dd2bd3cfc6f
Daria Myshlennikova <leannfster@gmail.com>
2025-04-05 19:49:47 +0200

    Решает задачу UiFormGroup

--- 05-ts/20-UiFormGroup/UiFormGroup.vue
+++ 05-ts/20-UiFormGroup/UiFormGroup.vue
@@ -1,13 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { Slot } from 'vue'
+
+const props = defineProps<{
+  for: string
+  label: string
+  description: string
+  hint: string
+  showHint: boolean
+  invalid: boolean
+}>()
+
+defineSlots<{
+  default: Slot
+  label?: Slot
+  description: Slot
+}>()
+const customFor = computed(() => (props.for))
+const isHintVisible = computed(() => (props.showHint || props.invalid))
+</script>
 
 <template>
   <div class="form-group">
     <div class="form-group__label-wrapper">
-      <label for="FOR" class="form-group__label">LABEL</label>
-      <div class="form-group__description">DESCRIPTION</div>
+      <label :for="customFor" class="form-group__label">
+        <slot name="label">{{ label }}</slot>
+      </label>
+      <div class="form-group__description">
+        <slot name="description">{{ description }}</slot>
+      </div>
     </div>
-    <div class="form-group__control">CONTENT</div>
-    <div class="form-group__hint form-group__hint--invalid">HINT | ERROR</div>
+    <div class="form-group__control">
+      <slot/>
+    </div>
+    <div
+      v-if="hint"
+      class="form-group__hint"
+      :class="{'form-group__hint--invalid': invalid}"
+      v-text="isHintVisible ? hint : ''"
+    ></div>
   </div>
 </template>
 
